e66d3f2afa440bb3ded8f759040b9224b4b20d00
Joel Martinez <42950020+JMartinez0404@users.noreply.github.com>
2022-06-13 19:26:39 -0500

    Comments out staticfiles_dirs which may be causing build failure

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # Used in development
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media')
-]
+]'''
 
 STATIC_URL = "/static/"
 
