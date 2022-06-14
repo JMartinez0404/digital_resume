6eeedb1c1e8978f8fca6bc92a55b579520884327
Joel Martinez <42950020+JMartinez0404@users.noreply.github.com>
2022-06-13 20:20:46 -0500

    Debugging static files build error

--- digital_resume/settings.py
+++ digital_resume/settings.py
@@ -136,14 +136,12 @@ USE_TZ = True
 ]'''
 
 STATIC_URL = "/static/"
-
 # Used in production
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-
 # Used in production
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
